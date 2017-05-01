789761c6a4f01dbb026f58397d8d28479538bbbb
Shehani Perera <shehaniperera7777@gmail.com>
2017-04-30 20:03:11 -0700

    Add files via upload
    Gas sensor code committed

--- sketch_finalGas.ino
+++ sketch_finalGas.ino
@@ -1,8 +1,8 @@
 
-int ledGreenLight = 11;
-int ledRedLight = 12;
-int smokeAnalogPin = A5;
-int alarmBuzzer = 10;
+int ledGreenLight = 11; // led green light - connected to arduino pin11
+int ledRedLight = 12; // led red light - connectde to arduino pin12
+int smokeAnalogPin = A5; //
+int alarmBuzzer = 10; // alarm is connected to arduino pin10
 
 // the thresh hold value
 int sensorThresholdVal = 400; //400
@@ -19,21 +19,21 @@ void loop() {
   int analogSensorData = analogRead(smokeAnalogPin); // read the analog input pin no A5
 
  
-  Serial.println(analogSensor); // pronts out the sensor read value
+  Serial.println(analogSensor); // prints out the sensor read value
 
   // checks if the analog sesor data has reached the thresh hold value
   if (analogSensorData  > sensorThresholdVal)
   {
-    digitalWrite(ledRedLight, HIGH);
+    digitalWrite(ledRedLight, HIGH); // led red light will blink
     digitalWrite(ledGreenLight, LOW);
-    tone(alarmBuzzer, 1000, 200);
+    tone(alarmBuzzer, 1000, 200); // alrm will start to buz
     
   }
   else
   {
-    digitalWrite(ledRedLight, LOW);
-    digitalWrite(ledGreenLight, HIGH);
-    noTone(alarmBuzzer);
+    digitalWrite(ledRedLight, LOW); 
+    digitalWrite(ledGreenLight, HIGH); // led green light will blink
+    noTone(alarmBuzzer); 
   }
   delay(100); // waits for 100 milliseconds to read the next input
 }
