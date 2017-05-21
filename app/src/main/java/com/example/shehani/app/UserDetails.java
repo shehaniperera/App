b9eecf503eb43aa16c15062dc6343c940a7ba9d4
shehaniperera <shehaniperera7777@gmail.com>
2017-05-21 23:03:17 +0530

    Android bluetooth code updated, TemperatureConversion updated

--- app/src/main/java/com/example/shehani/app/UserDetails.java
+++ app/src/main/java/com/example/shehani/app/UserDetails.java
@@ -2,12 +2,26 @@ package com.example.shehani.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserDetails extends AppCompatActivity {
+
+    EditText name,phone,age;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+
+        name = (EditText)findViewById(R.id.uname);
+        phone = (EditText)findViewById(R.id.no);
+        age =  (EditText)findViewById(R.id.age);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 }
